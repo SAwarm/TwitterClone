7f6f5dcc8b6ce469452923ba7442ac9fc3e404ae
SAwarm <jonasdasilvaelias@gmail.com>
2022-01-15 00:09:44 -0300

    ADD: LogOut method

--- App/Controllers/AuthController.php
+++ App/Controllers/AuthController.php
@@ -6,22 +6,36 @@ namespace App\Controllers;
 use MF\Model\Container;
 use MF\Controller\Action;
 
-class AuthController extends Action {
+class AuthController extends Action
+{
 
     public function authenticate()
     {
         $user = Container::getModel('User');
-        
+
         $user->__set('email', $_POST['email']);
         $user->__set('password', $_POST['password']);
 
         $return = $user->authenticate();
 
         if ($return) {
-            echo "Authenticate";
-            return;
+            session_start();
+
+            $_SESSION['id'] = $user->__get('id');
+            $_SESSION['name'] = $user->__get('name');
+
+            return header('Location: /timeline');
         }
 
-        header('Location: ../?login=error');
+        return header('Location: /?login=error');
+    }
+
+    public function logout()
+    {
+        session_start();
+
+        session_destroy();
+
+        return header('Location: /');
     }
 }
