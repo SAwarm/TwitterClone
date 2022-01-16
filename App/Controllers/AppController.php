9ce8f19aaa406b9f1ec65f59d2650eb7a5da5a14
SAwarm <jonasdasilvaelias@gmail.com>
2022-01-16 16:03:25 -0300

    ADD: actions for timeline and recover methods for model Tweet

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -13,7 +13,34 @@ class AppController extends Action
         session_start();
 
         if (!empty($_SESSION)) {
+
+            $tweet = Container::getModel('Tweet');
+
+            $tweet->__set('id_user', $_SESSION['id']);
+
+            $tweets = $tweet->getAll();
+
+            $this->view->tweets = $tweets;
+
             return $this->render('timeline');
+        }
+
+        return header('Location: /?login=error');
+    }
+
+    public function tweet()
+    {
+        session_start();
+
+        if (!empty($_SESSION)) {
+            $tweet = Container::getModel('Tweet');
+
+            $tweet->__set('tweet', $_POST['tweet']);
+            $tweet->__set('id_user', $_SESSION['id']);
+
+            $tweet->save();
+
+            return header('Location: /timeline');
         }
 
         return header('Location: /?login=error');
