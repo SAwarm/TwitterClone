032d85c30ff83fa8fa47bf0441a0b46f596db774
SAwarm <jonasdasilvaelias@gmail.com>
2022-01-16 16:02:16 -0300

    ADD: new model Tweet, insert and get tweets

---
+++ App/Models/Tweet.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Models;
+
+use MF\Model\Model;
+use PDO;
+
+class Tweet extends Model
+{
+
+    private $id;
+
+    private $id_user;
+
+    private $tweet;
+
+    private $data;
+
+    public function __get($atribute)
+    {
+        return $this->$atribute;
+    }
+
+    public function __set($atribute, $value)
+    {
+        $this->$atribute = $value;
+    }
+
+    public function save()
+    {
+        $query = "INSERT INTO tweets (id_user, tweet) VALUES (:id_user, :tweet)";
+
+        $stmt = $this->db->prepare($query);
+
+        $stmt->bindValue(':id_user', $this->__get('id_user'));
+        $stmt->bindValue(':tweet', $this->__get('tweet'));
+
+        $stmt->execute();
+
+        return $this;
+    }
+
+    public function getAll()
+    {
+        $query = "SELECT 
+                        t.id, 
+                        t.id_user,
+                        u.name,
+                        t.tweet, 
+                        DATE_FORMAT(t.data, '%d/%m/%Y %H:%i') as data
+                    FROM
+                        tweets as t
+                    LEFT JOIN 
+                        users as u
+                    ON
+                        t.id_user = u.id
+                    WHERE
+                        id_user = :id_user
+                    ORDER BY
+                        t.data DESC";
+
+        $stmt = $this->db->prepare($query);
+
+        $stmt->bindValue(':id_user', $this->__get('id_user'));
+
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
