6fd4b91acd7b756ad2d3528ed45468d725b9e6c5
SAwarm <jonasdasilvaelias@gmail.com>
2022-01-16 16:08:10 -0300

    ADD: Validation auth

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -12,35 +12,39 @@ class AppController extends Action
     {
         session_start();
 
-        if (!empty($_SESSION)) {
+        $this->validationAuth();
 
-            $tweet = Container::getModel('Tweet');
+        $tweet = Container::getModel('Tweet');
 
-            $tweet->__set('id_user', $_SESSION['id']);
+        $tweet->__set('id_user', $_SESSION['id']);
 
-            $tweets = $tweet->getAll();
+        $tweets = $tweet->getAll();
 
-            $this->view->tweets = $tweets;
+        $this->view->tweets = $tweets;
 
-            return $this->render('timeline');
-        }
-
-        return header('Location: /?login=error');
+        return $this->render('timeline');
     }
 
     public function tweet()
     {
         session_start();
 
+        $this->validationAuth();
+        
+        $tweet = Container::getModel('Tweet');
+
+        $tweet->__set('tweet', $_POST['tweet']);
+        $tweet->__set('id_user', $_SESSION['id']);
+
+        $tweet->save();
+
+        return header('Location: /timeline');
+    }
+
+    public function validationAuth()
+    {
         if (!empty($_SESSION)) {
-            $tweet = Container::getModel('Tweet');
-
-            $tweet->__set('tweet', $_POST['tweet']);
-            $tweet->__set('id_user', $_SESSION['id']);
-
-            $tweet->save();
-
-            return header('Location: /timeline');
+            return true;
         }
 
         return header('Location: /?login=error');
